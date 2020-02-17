42b81e629765b43992a48cc76e6b03db67ca5b5b
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-02-17 14:33:27 -0500

    part 5 with all test cases passing

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,37 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import static com.zipcodewilmington.assessment1.part1.BasicStringUtils.reverse;
+
 public class Palindrome {
 
+
     public Integer countPalindromes(String input){
-        return null;
+
+        // Checks for single instances of letter
+        int counter = 0;
+        if(input==null) {
+            return counter;
+        }
+        counter += input.length();
+
+        // Checks all substrings using StringBuilder
+        for (int i = 0; i < input.length() ; i++) {
+            for (int j = i+1; j < input.length(); j++) {
+
+                StringBuilder string = new StringBuilder(input.substring(i, j+1));
+                if(string.toString().equals(string.reverse().toString())) {
+                    counter++;
+                }
+
+            }
+        }
+
+        return counter;
+
     }
+
 }
+
+
+
+
