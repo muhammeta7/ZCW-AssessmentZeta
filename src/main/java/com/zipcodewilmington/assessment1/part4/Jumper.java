86c85649d3929464a2960e5abb8f332fa1328787
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-02-17 13:04:08 -0500

    pass all tests for part 4

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,6 +6,26 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
-        return -1;
+
+        int totalJumps= 0;
+
+        // Test Case 3
+        if(j > k) {
+            return k;
+        }
+
+        // Minimum of jumps before going past flag
+        int minJumps = (int)Math.floor(k/j);
+
+        // totalJump increments while sum of both types of jumps are less than height
+        while(totalJumps + (minJumps * j) < k) {
+            totalJumps++;
+        }
+
+        // Sum both after incrementing
+        totalJumps += minJumps;
+
+        return totalJumps;
+
     }
 }
