25356e9ac1af21b108e06367fd49d79926833ecb
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-02-17 09:59:49 -0500

    passed all integer utils

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.assessment1.part1;
 
+
+import static com.zipcodewilmington.assessment1.part1.BasicStringUtils.reverse;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -11,7 +14,13 @@ public class IntegerUtils {
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+
     }
 
     /**
@@ -19,7 +28,12 @@ public class IntegerUtils {
      * @return the product of all integers between 0 and not including `n`
      */
     public static Integer getProductOfN(Integer n) {
-        return null;
+
+        int product = 1;
+        for (int i = 1; i <= n; i++) {
+            product *= i;
+        }
+        return product;
     }
 
     /**
@@ -27,6 +41,7 @@ public class IntegerUtils {
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+        return Integer.valueOf(reverse(val.toString()));
     }
+
 }
